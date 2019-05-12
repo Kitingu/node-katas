5102468b75dcac00de56109f8bc3cdbf066de024
benlegendj <benlegendj@gmail.com>
2019-05-12 22:36:47 +0300

    use for loop for string reversing

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// soln one 
+// const reverse = ((str) => {
+//     const arr = str.split('')
+//     arr.reverse()
+//     return arr.join('') //join array elements into string
+
+// })
+
+
+// soln2
+const reverse = ((str)=>{
+    let reversed = ""
+    for (let character of str){
+        reversed = character + reversed //add the character to the start of the reversed string
+    }
+    return reversed
+
+})
+
 
 module.exports = reverse;
