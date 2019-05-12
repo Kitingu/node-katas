9855c8b0e9fed3c86c110168b844274d8b3a2e9d
benlegendj <benlegendj@gmail.com>
2019-05-12 23:26:35 +0300

    solve reverse int kata

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+const reverseInt = ((n) =>{
+
+    const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+
+    // add sign the long way
+    // if (n < 0){
+    //     return parseInt(reversed) * -1
+    // }
+    // return parseInt(reversed)
+
+    return parseInt(reversed) * Math.sign(n)
+})
 
 module.exports = reverseInt;
