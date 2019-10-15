a4af43df8661ee6590eb75909fb036d33ca213dc
benlegendj <benlegendj@gmail.com>
2019-10-15 14:40:06 +0300

    solve max character of a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = (str) => {
+	const chars = {};
+	let max = 0;
+	let maxcharacter = '';
+	for (let char of str) {
+		if (!chars[char]) {
+			chars[char] = 1;
+		} else {
+			chars[char]++;
+		}
+	}
+	for (let char in chars) {
+		if (chars[char] > max) {
+			max = chars[char];
+			maxcharacter = char;
+		}
+	}
+	return maxcharacter;
+	//short equivalent of the code in the first for loop
+	// chars[char] = chars[char] +1 || 1
+
+	//for of loop is used in iterables i.e strings and arrays
+	// for in is used in objects  i.e non iterables
+};
 
 module.exports = maxChar;
