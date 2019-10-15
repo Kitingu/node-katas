6e9ec696afa3de27adea22f89ba23bb48bed03cb
benlegendj <benlegendj@gmail.com>
2019-10-15 22:32:30 +0300

    solving anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const anagrams = (stringA, stringB) => {
+	let mapA = buildCharMap(stringA);
+	let mapB = buildCharMap(stringB);
+
+    // let mapA 
+    // let mapB
+	// for (let char of stringA.replace(/[^\w]/g, '').toLowerCase()) {
+	// 	if (!mapA[char]) {
+	// 		mapA[char] = 1;
+	// 	} else {
+	// 		mapA[char] += 1;
+	// 	}
+	// }
+
+	// for (let char of stringB.replace(/[^\w]/g, '').toLowerCase()) {
+	// 	if (!mapB[char]) {
+	// 		mapB[char] = 1;
+	// 	} else {
+	// 		mapB[char] += 1;
+	// 	}
+	// }
+	if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+		return false;
+	}
+
+	for (let char in mapA) {
+		if (mapA[char] !== mapB[char]) {
+			return false;
+		}
+	}
+	return true;
+};
+
+function buildCharMap(str) {
+	const charMap = {};
+	for (const char of str.replace(/[^\w]/g, '').toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	return charMap;
+}
 
 module.exports = anagrams;
